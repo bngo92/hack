4f87290df8c947440f7734826cdbb3b9ac35db95
Bryan Ngo <bngo92@gmail.com>
2014-09-19 15:26:12 -0700

    Added MusicHandler

--- server.py
+++ server.py
@@ -6,28 +6,38 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
-    self.write(open("client.html").read())
+    self.render("client.html")
+
+
+class MusicHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.set_header('Content-Type', 'audio/mpeg')
+    self.write(open('paradise.mp3', 'rb').read())
 
 
 class WSHandler(tornado.websocket.WebSocketHandler):
+  connections = set()
 
   def check_origin(self, origin):
     return True
 
   def open(self):
-    print 'new connection'
-    self.write_message("Hello World")
+    print('new connection')
+    WSHandler.connections.add(self)
 
   def on_message(self, message):
-    print 'message received %s' % message
-    self.write_message(message)
+    print('message received %s' % message)
+    for connection in WSHandler.connections:
+      connection.write_message(message)
 
   def on_close(self):
-    print 'connection closed'
+    print('connection closed')
+    WSHandler.connections.remove(self)
 
 
 application = tornado.web.Application([
   (r'/', MainHandler),
+  (r'/music', MusicHandler),
   (r'/ws', WSHandler),
 ])
 
